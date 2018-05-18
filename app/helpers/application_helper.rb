cdc23f3225998a4c2b625e6a771485d2b86f54cf
Eugene <bekinassoff@gmail.com>
2018-05-18 12:04:15 +0300

    gravatar added to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def avatar_url(user)
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?d=identicon&s=30"
+	end
+
 end
