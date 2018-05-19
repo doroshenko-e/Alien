faf0c80d900cb8f495b015d6f035c643fa99ea8c
Eugene <bekinassoff@gmail.com>
2018-05-19 16:55:39 +0300

    Devise errors shows in toastr

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,7 +3,8 @@ module DeviseHelper
 	def devise_error_messages!
   	flash_messages = []
 	  resource.errors.full_messages.each do |message|
-	   	text = '<script>toastr.error("#{message}");</script>'
+	  	message.to_sym
+	   	text = "<script>toastr.error('#{message}');</script>"
 	   	flash_messages << text.html_safe if message
 	  end
 	  flash_messages.join("\n").html_safe
