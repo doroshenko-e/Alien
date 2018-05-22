5c2e2f04d76257d2d9536b59d141d8fd6725e643
Eugene <bekinassoff@gmail.com>
2018-05-22 18:10:53 +0300

    Facebook omniauth working for sign in, not for sign up

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.5'
 # Devise gem for user authentification
 gem 'devise'
 gem 'toastr-rails'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
 
-  devise_for :users
+  devise_for 	:users, 
+  						:path => '', 
+  						:path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+  						:controllers => {:omniauth_callbacks => "omniauth_callbacks"}
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
