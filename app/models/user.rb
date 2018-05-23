f6edb082db28ed41338e8c64f0d73383a7e8a80e
Eugene <bekinassoff@gmail.com>
2018-05-23 12:48:07 +0300

    Login with facebook works

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,38 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable,  :omniauthable
+         :confirmable,  :omniauthable, omniauth_providers: [:facebook]
 
   validates :fullname, presence: true, length: {maximum: 50}
 
-  def self.from_omniauth(auth)
-  	user =User.where(email: auth.info.email).first
-  	if user
-  		return user
-  	else
-  		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  			user.provider = auth.provider
-  			user.uid = auth.info.email
-  			user.image = auth.info.image
-  			user.password = Devise.friendly_token[0,20]
-  		end 
-  	end
+  def self.find_for_oauth(auth)
+    user = User.where(email: auth.info.email, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        fullname: auth.info.name,
+        uid:      auth.uid,
+        provider: auth.provider,
+        image:    auth.info.image,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
   end
-  
+
+  def self.new_with_session(params, session)
+   super.tap do |user|
+     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+       user.email = data["email"] if user.email.blank?
+     end
+   end
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end  
 end
