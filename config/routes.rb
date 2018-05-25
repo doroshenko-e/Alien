99153de12559be42ff368e27169579b697567aaf
Eugene <bekinassoff@gmail.com>
2018-05-25 18:18:40 +0300

    begin to edit views

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,8 @@ Rails.application.routes.draw do
   devise_for 	:users, 
   						:path => '', 
   						:path_names => {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
-  						:controllers => {   omniauth_callbacks: "omniauth_callbacks",
-  																registrations: 'registrations'
-  														}
+  						:controllers => { omniauth_callbacks: 'omniauth_callbacks',
+  															registrations: 'registrations'}
 
   resources :users, only: [:show]
   resources :rooms
