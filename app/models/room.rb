c68b2826fa80a90c8d0df22a35278541a5d14104
Eugene <bekinassoff@gmail.com>
2018-06-26 14:54:59 +0300

    Reservation added to room show view

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   belongs_to :user
   has_many :images
+  has_many :reservations
 
   geocoded_by :address
   after_validation :geocode
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :rooms
-  resources :images, only: [:show, :create, :destroy]
+  resources :images
+
+  resources :room do
+    resources :reservations, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
